4d473c34c7e0e174c68981c8886a5b8ba11ca22e
Debrup7703 <debrupnandyoo7@gmail.com>
2024-07-26 19:17:51 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,19 @@
+
+
+
+public class Login extends JFrame {
+
+    Login()  {
+        getContentPane().setBackground(Color.WHITE);
+
+
+
+    }
+
+
+
+
+    public static void main(String[] args){
+        new Login();
+    }
+}
